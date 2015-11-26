de96b04c6aead030f13a2db7346280db4d97a968
marina4026 <marinad_25@hotmail.com>
2015-11-26 11:45:27 +0200

    hi

---
+++ login.php
@@ -0,0 +1,70 @@
+<?php
+include("config.php");
+session_start();
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+// username and password sent from Form
+$myusername=mysqli_real_escape_string($db,$_POST['username']);
+$mypassword=mysqli_real_escape_string($db,$_POST['password']);
+
+$sql="SELECT id FROM radar_biases WHERE username='$myusername' and password='$mypassword'";
+$result=mysqli_query($db,$sql);
+$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+$active=$row['active'];
+$count=mysqli_num_rows($result);
+ob_start(); // begin collecting output
+
+
+
+
+
+
+
+$choice="view";
+
+switch ($choice){
+case "add":
+include ("add.php");
+break;
+case "view":
+include ("view.php");
+break;
+case "edit":
+include ("edit.php");
+break;
+}
+// If result matched $myusername and $mypassword, table row must be 1 row
+if($count==1)
+{
+session_register("myusername");
+$_SESSION['login_user']=$myusername;
+
+header("location: welcome.php");
+}
+else
+{
+$error="Your Login Name or Password is invalid";
+}
+}
+
+
+?>
+<html>
+<body>
+
+<h1>Welcome to my home page!</h1>
+<p> Database.</p>
+
+<?php include 'footer.php';?>
+
+</body>
+</html> 
+<form action="" method="post">
+<label>UserName :</label>
+<input type="text" name="username"/><br />
+<label>Password :</label>
+<input type="password" name="password"/><br/>
+<input type="submit" value=" Submit "/><br />
+</form>
+
